d5ae94aa0aad90c5d7dfd1a1fe0fe29bfa06b7ce
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2026-01-11 16:57:36 -0500

    Add feed users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,4 +47,15 @@ RSpec.describe User, type: :model do
       expect(users(:john).initials).to eq 'JCY'
     end
   end
+
+  context '#feed_count' do
+    it 'returns the count of associated feeds' do
+      feed1 = Feed.create(name: 'Test Feed')
+      feed2 = Feed.create(name: 'Another Feed')
+      users(:jeremy).feed_users.create(feed: feed1)
+      users(:jeremy).feed_users.create(feed: feed2)
+
+      expect(users(:jeremy).feed_count).to eq 2
+    end
+  end
 end
